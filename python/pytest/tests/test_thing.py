c9fae659a1fc85244a3f6befc03d9466094ca304
Bastien Gallay <bastien@gallay.org>
2021-11-22 13:10:17 +0100

    end of session

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,11 +1,55 @@
 from app.thing import Thing
 
 
-def test_correct_greeting():
-    thing = Thing("Bob")
-    assert "Hello Bob!" == thing.return_hello_name()
+class Cell:
+    def __init__(self, _=False):
+        self.neighbours_number = 0
+        self.is_alive = False
+        self.compute_state()
+
+    def set_neighbours(self, neighbours_number):
+        self.neighbours_number = neighbours_number
+        self.compute_state()
+
+    def compute_state(self):
+        if self.neighbours_number==3:
+            self.is_alive = True
+
+class GameOfLife:
+    @staticmethod
+    def is_alive(cell):
+        if cell.neighbours_number == 3:
+            return True
+        return False
+
+    @staticmethod
+    def next_generation(cell):
+        return Cell()
 
 
-def test_fail():
-    thing = Thing("Albert")
-    assert "Wrong!" == thing.return_hello_name()
+# TODO : test with 0 neighbour
+def test_cell_should_die_if_only_one_neighbours():
+    cell = Cell()
+    cell.set_neighbours(1)
+    assert not GameOfLife.is_alive(cell)
+
+
+def test_game_of_life_should_return_dead_cell_if_only_one_neighbours():
+    cell = Cell()
+    cell.set_neighbours(1)
+    next_generation_cell = GameOfLife.next_generation(cell)
+    assert not next_generation_cell.is_alive
+
+
+# TODO : test with 2 neighbours
+def test_cell_should_survive_if_three_neighbours():
+    cell = Cell(True)
+    cell.set_neighbours(3)
+    assert GameOfLife.is_alive(cell)
+
+
+def test_dead_cell_is_set_alive_if_exactly_three_neighbours():
+    cell = Cell(False)
+    cell.set_neighbours(3)
+    next_generation_cell = GameOfLife.next_generation(cell)
+    assert next_generation_cell.is_alive
