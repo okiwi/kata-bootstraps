a5bcd34413c4b00828cf978c9bc313c8e484924e
Bastien Gallay <bastien@gallay.org>
2021-11-22 13:04:26 +0100

    end of session

--- fsharp/dotnet-core5/Kata.Tests/Tests.fs
+++ fsharp/dotnet-core5/Kata.Tests/Tests.fs
@@ -2,13 +2,23 @@ module Tests
 
 open FsUnit.Xunit
 open Kata
+open Kata.GameOfLife
 open Xunit
 
-[<Fact>]
-let ``Hello test using Xunit`` () =
-    Assert.Equal("Hello You", Say.hello "World")
+//[<Fact>]
+//let ``Hello test using Xunit`` () =
+//    Assert.Equal("Hello You", Say.hello "World")
 
 [<Fact>]
-let ``Hello world using FsUnit assertion library`` () =
-    Say.hello "World"
-    |> should equal "Hello You" 
+let ``Cell with three neighbours is alive``() =
+    let cell: Cell = Alive(1, 1)
+    let board: Cells = [ cell ]
+
+    GameOfLife.next_generation board cell |> should equal (Alive(1, 1))
+
+[<Fact>]
+let ``Dead cell with two neighbours is alive``() =
+    let cell: Cell = Dead
+    let board: Cells = [ cell ]
+
+    GameOfLife.next_generation board cell |> should equal Dead
--- fsharp/dotnet-core5/Kata/Library.fs
+++ fsharp/dotnet-core5/Kata/Library.fs
@@ -1,5 +1,11 @@
 namespace Kata
 
-module Say =
-    let hello name =
-        "Hello " + name
+module GameOfLife =
+    type Coordinates = int * int
+    type Cell = Alive of Coordinates | Dead
+    type Cells = Cell list
+    
+    type NextCellGenerator = Cells -> Cell -> Cell
+    
+    let next_generation: NextCellGenerator = fun cellList cell ->
+        cell
