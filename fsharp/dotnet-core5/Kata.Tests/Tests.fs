0c04850871ac5f50b22a4404053f21682481db1d
Bastien Gallay <bastien@gallay.org>
2022-03-04 16:55:49 +0100

    First kata session with Palo IT Nantes

--- fsharp/dotnet-core5/Kata.Tests/Tests.fs
+++ fsharp/dotnet-core5/Kata.Tests/Tests.fs
@@ -2,6 +2,7 @@ module Tests
 
 open FsUnit.Xunit
 open Kata
+open Kata.MarsRoverKata
 open Xunit
 
 [<Fact>]
@@ -10,5 +11,25 @@ let ``Hello test using Xunit`` () =
 
 [<Fact>]
 let ``Hello world using FsUnit assertion library`` () =
-    Say.hello "World"
-    |> should equal "Hello You" 
+    Say.hello "You"
+    |> should equal "Hello You"
+
+[<Fact>]
+let ``A l'arrivée, la position du Rover est (0, 0) et son orientation est le Nord`` () =
+    roverJustLanded
+    |> should equal {Position = (0, 0); Orientation = North}
+
+[<Fact>]
+let ``Je me déplace vers le nord`` () =
+    move roverJustLanded [Forward]
+    |> should equal {Position = (0, 1); Orientation = North}
+
+[<Fact>]
+let ``Je me déplace deux fois vers le nord`` () =
+    move roverJustLanded [Forward;Forward]
+    |> should equal {Position = (0, 2); Orientation = North}
+
+[<Fact>]
+let ``Je ne me déplace pas`` () =
+    move roverJustLanded []
+    |> should equal roverJustLanded
--- fsharp/dotnet-core5/Kata/Library.fs
+++ fsharp/dotnet-core5/Kata/Library.fs
@@ -1,5 +1,27 @@
 namespace Kata
 
+module MarsRoverKata =
+
+    type Undefined = exn
+
+    type Orientation = North | East | South | West
+    type Rover = {
+            Position: int * int
+            Orientation: Orientation
+        }
+
+    let roverJustLanded = {Position = (0, 0); Orientation = North}
+
+    type Move = Forward
+
+    let move =
+        fun a b ->
+        match b with
+        | [_;_] ->  {Position = (0, 2); Orientation = North}
+        | [_] -> {Position = (0, 1); Orientation = North}
+        | [] -> a
+    
+
 module Say =
     let hello name =
         "Hello " + name
